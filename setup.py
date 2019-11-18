7a071c4f42652419e914bf3b1549a185a3a388c8
cboissel <56295894+cboissel@users.noreply.github.com>
2019-11-19 00:16:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ package_dir = {k: os.path.join(here, k.replace(".", "/")) for k in packages}
 with open(os.path.join(here, "requirements.txt"), "r") as f:
     requirements = f.read().split("\n")
 
-setup(name='td2a_plotting',
+setup(name='ensae2019',
       version='0.1',
       description="Example of module to share plotting functions",
       long_description="Exemple de module python pour partager ses graphes.",
       author='Boissel Camille, Cazanave Thibaud, Cerles Sébastien, Chabriel Maxime, Fourchault Raphaël',
-      url='https://github.com/SebastienCerles/Amazing-Pollution?fbclid=IwAR2HhOJPM4McsnxmZU0uF3PGVsx1l4ZSe9wTO2goPJaOtrSsE27Lj7LA5nY',
+      url='https://github.com/cboissel/Amazing-Pollution/blob/master/setup.py',
       packages=packages,
       package_dir=package_dir,
       # requires indique quels packages doivent être installés
